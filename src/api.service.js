09d52322ab176db1d8da8b45b658973dae0b00cc
Shivani Verma <shivani.lko.247@gmail.com>
2022-07-12 17:35:11 +0530

    Updated Readme

--- src/api.service.js
+++ src/api.service.js
@@ -3,43 +3,43 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 
 // const API_URL = 'http://localhost:3333/api/v1/';
-const API_URL = 'http://mobidoctor.org:8033/api/v1/';
+const API_URL = 'http://someurl.com:9133/api/v1/';
 
 let APIService = axios.create({
-  baseURL: API_URL,
+    baseURL: API_URL,
 });
 
-if(sessionStorage.getItem('user')){
-  let user = sessionStorage.getItem('user');
-  user = JSON.parse(user)
-  let username =  user.username;
-  let pwd = atob(sessionStorage.getItem('pwd'))
-  APIService = axios.create({
-      baseURL: `${API_URL}/`,
-      auth: {
-          username,
-          password: pwd
-      }
-  });
+if (sessionStorage.getItem('user')) {
+    let user = sessionStorage.getItem('user');
+    user = JSON.parse(user)
+    let username = user.username;
+    let pwd = atob(sessionStorage.getItem('pwd'))
+    APIService = axios.create({
+        baseURL: `${API_URL}/`,
+        auth: {
+            username,
+            password: pwd
+        }
+    });
 }
 
 
-APIService.interceptors.request.use((config)=>{
-  Notiflix.Loading.circle('Loading/Sending data.');
-  return config;
-},(error) => {
-  Notiflix.Loading.remove();
-  Notiflix.Notify.failure(error.toString());
-  return Promise.reject(error);
+APIService.interceptors.request.use((config) => {
+    Notiflix.Loading.circle('Loading/Sending data.');
+    return config;
+}, (error) => {
+    Notiflix.Loading.remove();
+    Notiflix.Notify.failure(error.toString());
+    return Promise.reject(error);
 });
 
 APIService.interceptors.response.use((config) => {
-  Notiflix.Loading.remove();
-  return config;
-},(error) => {
-  Notiflix.Loading.remove();
-  Notiflix.Notify.failure(error.toString());
-  return Promise.reject(error);
+    Notiflix.Loading.remove();
+    return config;
+}, (error) => {
+    Notiflix.Loading.remove();
+    Notiflix.Notify.failure(error.toString());
+    return Promise.reject(error);
 });
 
 // configure({
